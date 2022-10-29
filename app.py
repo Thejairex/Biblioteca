84946fc8872d4147733f8baf6dd6f2fc8ee32739
Thejairex <yairjesus777@gmail.com>
2022-10-29 04:48:36 -0300

    Add Titles at the Pages

--- app.py
+++ app.py
@@ -7,57 +7,57 @@ from querys.querysNovelas import qNovela
 app = Flask(__name__)
 
 mysql = MySQL(app)
-app.config['MYSQL_HOST'] = 'Thejairex2.mysql.pythonanywhere-services.com'
-app.config['MYSQL_USER'] = 'Thejairex2'
-app.config['MYSQL_PASSWORD'] = 'Aiwa2015'
-app.config['MYSQL_DB'] = 'Thejairex2$biblioteca'
+# app.config['MYSQL_HOST'] = 'Thejairex2.mysql.pythonanywhere-services.com'
+# app.config['MYSQL_USER'] = 'Thejairex2'
+# app.config['MYSQL_PASSWORD'] = 'Aiwa2015'
+# app.config['MYSQL_DB'] = 'Thejairex2$biblioteca'
 app.secret_key = "OtakuTeca"
 
-# app.config['MYSQL_HOST'] = "localhost"
-# app.config['MYSQL_USER'] = "root"
-# app.config['MYSQL_PASSWORD'] = ""
-# app.config['MYSQL_DB'] = "biblioteca"
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "root"
+app.config['MYSQL_PASSWORD'] = ""
+app.config['MYSQL_DB'] = "biblioteca"
 
 @app.route("/")
 def index():
 	
-	return render_template("index.html")
+	return render_template("index.html" , titlePage="Biblioteca")
 
 @app.route("/animes")
 def animes():
 	data = qAnime.fetchall_anime()
-	return render_template("animes.html", datas=data)
+	return render_template("animes.html", datas=data, titlePage="Animes - Biblioteca")
 
 @app.route("/comics")
 def comics():
 	data = qComcic.fetchall_comic()
-	return render_template("comics.html", datas=data)
+	return render_template("comics.html", datas=data, titlePage="Comics - Biblioteca")
 
 @app.route("/novelas")
 def Novelas():
 	data = qNovela.fetchall_novela()
-	return render_template("novelas.html", datas=data)
+	return render_template("novelas.html", datas=data, titlePage="Novelas - Biblioteca")
 
 @app.route("/comic" , methods=['POST'])
 def comic():
 	if request.method == 'POST':
 		id = request.form['id']
 		data = qComcic.fetchone_comic(id)
-		return render_template("comic.html", datas=data, serie="comic", titulo="Comic")
+		return render_template("comic.html", datas=data, serie="comic", titulo="Comic", titlePage="Comic - Biblioteca")
 
 @app.route("/anime" , methods=['POST'])
 def anime():
 	if request.method == 'POST':
 		id = request.form['id']
 		data = qAnime.fetchone_anime(id)
-		return render_template("anime.html", datas=data)
+		return render_template("anime.html", datas=data, titlePage="Anime - Biblioteca")
 
 @app.route("/novela" , methods=['POST'])
 def novela():
 	if request.method == 'POST':
 		id = request.form['id']
 		data = qNovela.fetchone_novela(id)
-		return render_template("comic.html", datas=data,  serie="novela", titulo="Novela")
+		return render_template("comic.html", datas=data,  serie="novela", titulo="Novela", titlePage="Novela - Biblioteca")
 
 
 if __name__ == '__main__':
